5e312a83f457582663d1d15b8f9998bae743a5b7
Matheus Duarte <matheusduartegalvao@gmail.com>
2023-08-21 22:05:31 -0300

    initial commit

---
+++ app/Http/Controllers/ExampleController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExampleController extends Controller
+{
+    public function homepage() {
+        $ourName = 'Matheus';
+        $animals = ['Bipo', 'Loro', 'Pretume'];
+
+        return view('homepage', ['allAnimals' => $animals, 'name' => $ourName]);
+    }
+
+    public function aboutPage() {
+        return view('single-post');
+    }
+}
